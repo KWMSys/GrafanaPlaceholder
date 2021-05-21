14e4e202271c02bd84976ee0667b6d49c64d890a
Marek Schmidt <info@msdev.me>
2021-05-21 19:31:18 +0200

    Implemented hide on mobile

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -2,13 +2,39 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import { stylesFactory } from '@grafana/ui';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme();
+export const SimplePanel: React.FC<Props> = (props) => {
+  let { options, width, height } = props;
   const styles = getStyles();
+
+  // This is a hacky way of getting there... But if works ¯\_(ツ)_/¯
+  if (!options.showMobile) {
+    let style = document.getElementById('custom-styles-panel-' + props.id);
+    if (!style) {
+      style = document.createElement('style');
+      style.id = 'custom-styles-panel-' + props.id;
+      document.head.appendChild(style);
+    }
+    style.innerHTML = `
+    @media (max-width: 768px) {
+      div#panel-${props.id} { 
+        z-index: -99999999!important;
+        height: 0px!important;
+        overflow: hidden!important;
+        display: none!important;
+      }
+    }
+    `;
+  } else {
+    let style = document.getElementById('custom-styles-panel-' + props.id);
+    if (style) {
+      style.parentNode?.removeChild(style);
+    }
+  }
+
   return (
     <div
       className={cx(
@@ -18,33 +44,7 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
           height: ${height}px;
         `
       )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
-
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
-        )}
-        <div>Text option value: {options.text}</div>
-      </div>
-    </div>
+    ></div>
   );
 };
 
@@ -52,17 +52,6 @@ const getStyles = stylesFactory(() => {
   return {
     wrapper: css`
       position: relative;
-    `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
     `,
   };
 });
